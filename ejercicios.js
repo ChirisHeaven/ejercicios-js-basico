eb9da40a4eef1871cd797b1dc0ec65fc1adb9d41
ChrisHeaven <108626482+ChirisHeaven@users.noreply.github.com>
2026-05-08 10:14:36 -0400

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -34,10 +34,10 @@ let bNumero = 3;
 console.log("Suma string + number:", aCombinado + bNumero);
 console.log("Resta string + number:", aCombinado - bNumero);
 
-// 5. Variable edad = 15
-let edad = 15;
-console.log("¿Es mayor a 10?:", edad > 10);
-console.log("¿Es menor a 18?:", edad < 18);
+// 5. Variable edad = 27
+let edad = 27;
+console.log("¿Es mayor a 15?:", edad > 15);
+console.log("¿Es menor a 30?:", edad < 30);
 
 // 6. Variable numero = 8
 let numero = 8;
